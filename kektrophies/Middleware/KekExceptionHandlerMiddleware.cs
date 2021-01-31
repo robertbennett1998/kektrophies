409b711e9399de0a0404b3623ac6baaade034d49
Robert Bennett <robertbennett1998@outlook.com>
2021-01-31 19:44:56 +0000

    Added the exception handler middleware

---
+++ kektrophies/Middleware/KekExceptionHandlerMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using kektrophies.Exceptions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace kektrophies.Middleware
+{
+    public class KekExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public KekExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var endpoint = httpContext.Features.Get<IEndpointFeature>()?.Endpoint;
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (KekException e)
+            {
+                httpContext.Response.StatusCode = (int)e.StatusCode;
+                await httpContext.Response.WriteAsync(e.Message);
+            }
+        }
+    }
+
+    public static class StatusExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseKekExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<KekExceptionHandlerMiddleware>();
+        }
+    }
+}
