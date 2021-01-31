78cb671ccac56e7d24f2ae647374fc4bdf9a7c3a
Robert Bennett <robertbennett1998@outlook.com>
2021-01-31 18:47:31 +0000

    Added API path and a selection to use MySql instead of SqlServer

--- kektrophies/ClientApp/src/Config.js
+++ kektrophies/ClientApp/src/Config.js
@@ -1,7 +1,5 @@
-const useLocal = true;
-
 const Config = { 
-    apiPath : useLocal ? "http://127.0.0.1:5000/api/" : undefined
+    apiPath : !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "http://46.252.205.196/plesk-site-preview/kektrophies.co.uk/api/" : "http://127.0.0.1:5000/api/"
 }
 
 export default Config;
