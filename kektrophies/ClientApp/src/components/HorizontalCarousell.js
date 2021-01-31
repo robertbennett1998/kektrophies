206f36f15032c52a2c3ca0a875d729d5af0dd9e1
Robert Bennett <robertbennett1998@outlook.com>
2021-01-31 15:57:58 +0000

    Refactored HorizontalCarousell as a stateful component

--- kektrophies/ClientApp/src/components/HorizontalCarousell.js
+++ kektrophies/ClientApp/src/components/HorizontalCarousell.js
@@ -1,85 +1,110 @@
-import React, { useEffect, useState } from 'react';
-import withWidth from '@material-ui/core/withWidth';
+import React from 'react';
 import { IconButton } from '@material-ui/core';
 import BodyContainer from './BodyContainer';
 import BodyItem from './BodyItem';
 import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import withWidth from '@material-ui/core/withWidth';
 
-const HorizontalCarousell = React.memo((props) => {  
-    const [startIndex, _setStartIndex] = useState(0);
-    const [maxItemsToDisplay, _setMaxItemsToDisplay] = useState(0);
-
-    //console.log(props.data, props.renderFunction);
-    const setStartIndex = (index) => {
-        if (props.data === null || props.data === undefined)
+export class HorizontalCarousell extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            startIndex: 0,
+            maxItemsToDisplay: 0
+        }
+    }
+    
+    setStartIndex(index) {
+        if (this.props.data === null || this.props.data === undefined)
             return;
 
         if (index < 0)
             index = 0;
 
-        if (index > props.data.length - maxItemsToDisplay)
-            index = props.data.length - maxItemsToDisplay;
+        if (index > this.props.data.length - this.state.maxItemsToDisplay)
+            index = this.props.data.length - this.state.maxItemsToDisplay;
 
-        _setStartIndex(index);
-    };
+        this.setState({ 
+            startIndex: index
+        });
+    }
 
-    useEffect(() => {
-        var maxToDisplay = 0;
-        var exitOnSet = false;
-        const sizeKeys = ["xl", "lg", "md", "sm", "xs"];
-
-        //console.log("Width", props.width);
-        for (var sizeKey of sizeKeys)
-        {
-            if (sizeKey === props.width)
-                exitOnSet = true;
-
-            if (props.sizeToMaxItemsToDisplayMap[sizeKey] !== undefined)
-            {
-                maxToDisplay = props.sizeToMaxItemsToDisplayMap[sizeKey];
-
-                if (exitOnSet)
-                    break;
-            }
-        }
-
-        setStartIndex(startIndex, maxToDisplay);
-        _setMaxItemsToDisplay(maxToDisplay);
-    }, [setStartIndex, startIndex, props]);
+    setMaxItemsToDisplay(maxItems) {
+        this.setStartIndex(this.state.startIndex);
+        this.setState({
+            maxItemsToDisplay: maxItems
+        });
+    }
     
-    return (
-                <BodyContainer>
-                    <BodyItem xs={1}>
-                        <div style={{"height": "100%", "display": "inline-grid", "justifyContent": "center", "alignContent": "center"}}>
-                            <IconButton style={{"transform": "scale(-2)"}} onClick={() => setStartIndex(startIndex - 1)}>
-                                <DoubleArrowIcon />
-                            </IconButton>
-                        </div>
-                    </BodyItem>
-                    <BodyItem xs={10}>
-                        <BodyContainer>
-                            {                 
-                                props.data?.map((data, index) => {
-                                    if (index >= startIndex && index < startIndex + maxItemsToDisplay)
-                                    {
-                                        //console.log("MAX", maxItemsToDisplay); 
-                                        return props.renderFunction(data, index);
-                                    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.data !== this.props.data || prevProps.width !== this.props.width) {
+            let maxToDisplay = 0;
+            let exitOnSet = false;
+            const sizeKeys = ["xl", "lg", "md", "sm", "xs"];
 
-                                    return undefined;
-                                })
-                            }
-                        </BodyContainer>
-                    </BodyItem>                    
-                    <BodyItem xs={1}>
-                        <div style={{"height": "100%", "display": "inline-grid", "justifyContent": "center", "alignContent": "center", "float": "right"}}>
-                            <IconButton style={{"transform": "scale(2)"}} onClick={() => setStartIndex(startIndex + 1)}>
-                                <DoubleArrowIcon />
-                            </IconButton>
-                        </div>
-                    </BodyItem>             
-                </BodyContainer>
-            );
-});
+            console.log("Width", this.props.width);
+            for (const sizeKey of sizeKeys) {
+                if (sizeKey === this.props.width)
+                    exitOnSet = true;
+
+                if (this.props.sizeToMaxItemsToDisplayMap[sizeKey] !== undefined) {
+                    maxToDisplay = this.props.sizeToMaxItemsToDisplayMap[sizeKey];
+
+                    if (exitOnSet)
+                        break;
+                }
+            }
+
+            this.setMaxItemsToDisplay(maxToDisplay);
+        }
+    }
+    
+    render() {
+        return (
+            <BodyContainer>
+                <BodyItem xs={1}>
+                    <div style={{
+                        "height": "100%",
+                        "display": "inline-grid",
+                        "justifyContent": "center",
+                        "alignContent": "center"
+                    }}>
+                        <IconButton style={{"transform": "scale(-2)"}} onClick={() => this.setStartIndex(this.state.startIndex - 1)}>
+                            <DoubleArrowIcon/>
+                        </IconButton>
+                    </div>
+                </BodyItem>
+                <BodyItem xs={10}>
+                    <BodyContainer>
+                        {
+                            this.props.data?.map((data, index) => {
+                                if (index >= this.state.startIndex && index < this.state.startIndex + this.state.maxItemsToDisplay) {
+                                    //console.log("MAX", maxItemsToDisplay); 
+                                    return this.props.renderFunction(data, index);
+                                }
+    
+                                return undefined;
+                            })
+                        }
+                    </BodyContainer>
+                </BodyItem>
+                <BodyItem xs={1}>
+                    <div style={{
+                        "height": "100%",
+                        "display": "inline-grid",
+                        "justifyContent": "center",
+                        "alignContent": "center",
+                        "float": "right"
+                    }}>
+                        <IconButton style={{"transform": "scale(2)"}} onClick={() => this.setStartIndex(this.state.startIndex + 1)}>
+                            <DoubleArrowIcon/>
+                        </IconButton>
+                    </div>
+                </BodyItem>
+            </BodyContainer>
+        );
+    }
+}
 
 export default withWidth()(HorizontalCarousell);
