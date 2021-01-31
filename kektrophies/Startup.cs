ceb188fbdf4abb236bc6f0e14347631aa7988745
Robert Bennett <robertbennett1998@outlook.com>
2021-01-31 23:58:54 +0000

    Attempting to see if issue is due to SPA

--- kektrophies/Startup.cs
+++ kektrophies/Startup.cs
@@ -34,7 +34,7 @@ namespace kektrophies
             services.AddSingleton<ICryptoService, CryptoService>();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            // services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,15 +74,15 @@ namespace kektrophies
                 endpoints.MapControllers();
             });
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
+            // app.UseSpa(spa =>
+            // {
+            //     spa.Options.SourcePath = "ClientApp";
+            //
+            //     if (env.IsDevelopment())
+            //     {
+            //         spa.UseReactDevelopmentServer(npmScript: "start");
+            //     }
+            // });
         }
     }
 }
