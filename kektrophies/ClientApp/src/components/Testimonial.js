968dd25b9e1e262c5d1526736f98cfa6b9e05389
Robert Bennett <robertbennett1998@outlook.com>
2021-02-01 20:03:03 +0000

    Removed the rendering of the index from testimonials.

--- kektrophies/ClientApp/src/components/Testimonial.js
+++ kektrophies/ClientApp/src/components/Testimonial.js
@@ -4,7 +4,7 @@ import BodyItem from './BodyItem';
 const Testimonial = ({testimonialData, index}) => {
     return (
                 <BodyItem paper fill xs={12} sm={6} md={4} lg={3} xl={2}>  
-                    <h1>{testimonialData.firstName} {testimonialData.lastName} {index}</h1>
+                    <h1>{testimonialData.firstName} {testimonialData.lastName}</h1>
                     <h3>{testimonialData.description}</h3>
                     <p>{testimonialData.testimonial}</p>
                 </BodyItem>
